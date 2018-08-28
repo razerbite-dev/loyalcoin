cad9780c15109f3c646a0db5127881076a0e1123
Anthony Yolach Lloveras <anthonylloveras.boxtypd@gmail.com>
2018-08-29 00:43:42 +0800

    added temporary disable safe updates for sql

--- db_migrate_script.sql
+++ db_migrate_script.sql
@@ -1,3 +1,5 @@
+
+SET SQL_SAFE_UPDATES = 0;
 UPDATE wp_options SET option_value = replace(option_value, 'http://loyalcoin.razerbite.com', 'http://lylv2.loyalcoin.io') WHERE option_name = 'home' OR option_name = 'siteurl';
 
 UPDATE wp_posts SET guid = replace(guid, 'http://loyalcoin.razerbite.com','http://lylv2.loyalcoin.io');
@@ -5,3 +7,4 @@ UPDATE wp_posts SET guid = replace(guid, 'http://loyalcoin.razerbite.com','http:
 UPDATE wp_posts SET post_content = replace(post_content, 'http://loyalcoin.razerbite.com', 'http://lylv2.loyalcoin.io');
 
 UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://loyalcoin.razerbite.com','http://lylv2.loyalcoin.io');
+SET SQL_SAFE_UPDATES = 1;
